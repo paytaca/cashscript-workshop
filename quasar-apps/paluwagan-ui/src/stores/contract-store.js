c5835154a870ac426437826cb2d70bca3d001fa3
khirvy019 <khirvy_19@yahoo.com>
2025-08-31 22:40:05 +0800

    populate contract store data & actions

--- quasar-apps/paluwagan-ui/src/stores/contract-store.js
+++ quasar-apps/paluwagan-ui/src/stores/contract-store.js
@@ -1,13 +1,44 @@
 import { defineStore } from 'pinia'
+import { compileContract } from 'src/lib/contract'
 
 export const useContractStore = defineStore('contract', {
   state: () => ({
     // Data about contract: add parameters, address, balance
+    address: '',
+    parameters: {
+      members: [],
+      potAmount: 0,
+      period: 0,
+    },
+    balance: 0,
   }),
   getters: {
     // provide getter functions
+    contractInstance() {
+      return compileContract(
+        this.parameters.members,
+        this.parameters.potAmount,
+        this.parameters.period,
+      )
+    }
   },
   actions: {
     // Add actions: loadContract(...parameters), refreshBalance()
+    saveContract(members, potAmount, period) {
+      const contract = compileContract(members, potAmount, period);
+      this.$patch({
+        address: contract.address,
+        parameters: {
+          members,
+          potAmount: parseInt(potAmount),
+          period: parseInt(period),
+        },
+        balance: 0,
+      })
+    },
+    async refreshBalance() {
+      const balance = await this.contractInstance.getBalance();;
+      this.balance = parseInt(balance);
+    }
   },
 })
