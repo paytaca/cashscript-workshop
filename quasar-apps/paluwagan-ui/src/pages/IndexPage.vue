c5c33799fa073bebf47ad72ea50934e1af806374
LukeHeimen <kdlanante@up.edu.ph>
2025-08-31 10:56:56 +0800

    Add members list card for displaying members

--- quasar-apps/paluwagan-ui/src/pages/IndexPage.vue
+++ quasar-apps/paluwagan-ui/src/pages/IndexPage.vue
@@ -4,6 +4,9 @@
      <div class="row col-12 justify-center">
        <pool-balance-card />
      </div>
+     <div class="row col-12 justify-center">
+       <members-list-card />
+     </div>
     <send-to-pool-button />
     <scan-button />
   </div>
@@ -15,6 +18,7 @@ import { defineComponent } from 'vue'
 import SendToPoolButton from 'src/components/buttons/SendToPoolButton.vue'
 import ScanButton from 'src/components/buttons/ScanButton.vue'
 import PoolBalanceCard from 'src/components/cards/PoolBalanceCard.vue'
+import MembersListCard from 'src/components/cards/MembersListCard.vue'
 
 export default defineComponent({
   name: 'IndexPage',
@@ -22,7 +26,8 @@ export default defineComponent({
   components: {
     SendToPoolButton,
     ScanButton,
-    PoolBalanceCard
+    PoolBalanceCard,
+    MembersListCard
   },
 
   data() {
