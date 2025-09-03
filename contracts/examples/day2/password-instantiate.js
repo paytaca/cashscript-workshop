adb7f6bce1e69eab970fc0a9b4d7f7a1736514f3
LukeHeimen <kdlanante@up.edu.ph>
2025-09-03 17:41:37 +0800

    Rename password-compile to password-instantiate

--- contracts/examples/day2/password-instantiate.js
+++ contracts/examples/day2/password-instantiate.js
@@ -8,5 +8,5 @@ const contractArgs = ['change_or_keep_password'] // change or keep password
 // set up network provider
 const provider = new MockNetworkProvider()
 
-// compile contract
+// instantiate contract
 const contract = new Contract(PasswordArtifact, contractArgs, { provider })
