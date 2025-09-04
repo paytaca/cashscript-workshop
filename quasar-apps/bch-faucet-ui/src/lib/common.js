509877a7dcc1c9187c4f01c7eb96f25c277cd221
LukeHeimen <kdlanante@up.edu.ph>
2025-09-04 12:57:16 +0800

    Add comments to addressToPkhash function

--- quasar-apps/bch-faucet-ui/src/lib/common.js
+++ quasar-apps/bch-faucet-ui/src/lib/common.js
@@ -1,6 +1,11 @@
 import { decodeCashAddress } from "@bitauth/libauth";
 import { ElectrumNetworkProvider } from "cashscript";
 
+/**
+ * Decodes the given BCH address to its equivalent public key hash.
+ * @param {String} address the given BCH address
+ * @returns the public key hash decoded from the address
+ */
 export function addressToPkhash(address) {
   const decoded = decodeCashAddress(address)
   if (typeof decoded === 'string') throw new Error(decoded);
