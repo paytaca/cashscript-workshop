5b7d93678bab5b0a546690bf3394f088d0cc05f4
khirvy019 <khirvy_19@yahoo.com>
2025-09-08 10:51:44 +0800

    rename quasar example pages

--- quasar-apps/password-vault-ui/src/router/routes.js
+++ quasar-apps/password-vault-ui/src/router/routes.js
@@ -19,14 +19,14 @@ const routes = [
       { path: 'vue/inputs', component: () => import('src/pages/examples/vue/8_FormInputs.vue'), name: 'example-v-input' },
       { path: 'vue/components', component: () => import('src/pages/examples/vue/9_Components.vue'), name: 'example-v-components' },
 
-      { path: 'quasar/buttons', component: () => import('src/pages/examples/quasar/ButtonExamples.vue'), name: 'example-q-btn' },
-      { path: 'quasar/inputs', component: () => import('src/pages/examples/quasar/FormInputExamples.vue'), name: 'example-q-input' },
-      { path: 'quasar/classes', component: () => import('src/pages/examples/quasar/QuasarClasses.vue'), name: 'example-q-classes' },
-      { path: 'quasar/cards', component: () => import('src/pages/examples/quasar/CardExamples.vue'), name: 'example-q-card' },
-      { path: 'quasar/tables', component: () => import('src/pages/examples/quasar/TableExamples.vue'), name: 'example-q-table' },
-      { path: 'quasar/dialogs', component: () => import('src/pages/examples/quasar/DialogExamples.vue'), name: 'example-q-dialog' },
-      { path: 'quasar/tab-panels', component: () => import('src/pages/examples/quasar/TabPanelExamples.vue'), name: 'example-q-tab-panel' },
-      { path: 'quasar/list-items', component: () => import('src/pages/examples/quasar/ListItemExamples.vue'), name: 'example-q-list-item' },
+      { path: 'quasar/buttons', component: () => import('src/pages/examples/quasar/1_ButtonExamples.vue'), name: 'example-q-btn' },
+      { path: 'quasar/inputs', component: () => import('src/pages/examples/quasar/2_FormInputExamples.vue'), name: 'example-q-input' },
+      { path: 'quasar/classes', component: () => import('src/pages/examples/quasar/3_QuasarClasses.vue'), name: 'example-q-classes' },
+      { path: 'quasar/cards', component: () => import('src/pages/examples/quasar/4_CardExamples.vue'), name: 'example-q-card' },
+      { path: 'quasar/tables', component: () => import('src/pages/examples/quasar/5_TableExamples.vue'), name: 'example-q-table' },
+      { path: 'quasar/dialogs', component: () => import('src/pages/examples/quasar/6_DialogExamples.vue'), name: 'example-q-dialog' },
+      { path: 'quasar/tab-panels', component: () => import('src/pages/examples/quasar/8_TabPanelExamples.vue'), name: 'example-q-tab-panel' },
+      { path: 'quasar/list-items', component: () => import('src/pages/examples/quasar/7_ListItemExamples.vue'), name: 'example-q-list-item' },
 
       { path: 'apps/todo', component: () => import('src/pages/examples/apps/TodoApp.vue'), name: 'example-app-todo' },
     ]
