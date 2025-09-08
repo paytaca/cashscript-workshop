8b0a52370f48e829d267b2f3cf2a72c840e3e3b3
joemarct <joemar.ct@gmail.com>
2025-09-08 10:02:31 +0800

    Introduction slides (WIP)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./presentations/**/*.html",
+    "./presentations/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
