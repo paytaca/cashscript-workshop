543b5ce326139c0ac8a9c70f5a76731782aa83af
LukeHeimen <kdlanante@up.edu.ph>
2025-11-15 09:56:07 +0800

    Simplify day 2 part 1 slide activities to focus more on actual hands-on coding; simplify js file to use for activities

---
+++ quasar-apps/password-vault-ui/src/contracts/passvault-run.js
@@ -0,0 +1,5 @@
+import { Contract, ElectrumNetworkProvider } from "cashscript"
+import { addressToPkhash } from "../lib/common.js"
+// if with doesn't work, try assert
+import PasswordVaultArtifact from './PasswordVault.json' with { type: 'json' }
+
